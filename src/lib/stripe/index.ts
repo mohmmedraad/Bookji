8f22301b3b49ff8f25ef41fa109ae11bb33c165e
Mohamed Raad <mraad6689@gamil.com>
2024-02-03 14:57:58 +0300

    use env package

--- src/lib/stripe/index.ts
+++ src/lib/stripe/index.ts
@@ -1,6 +1,7 @@
+import { env } from "@/env.mjs"
 import Stripe from "stripe"
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+export const stripe = new Stripe(env.STRIPE_SECRET_KEY as string, {
     apiVersion: "2023-10-16",
     appInfo: {
         name: "Bookji",
