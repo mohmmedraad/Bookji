df4fdbe9b52f6f672ecaedb1a4fb6ed169fdab31
Mohamed Raad <mraad6689@gamil.com>
2024-04-03 06:44:14 +0300

    fix: rename the components

--- src/app/(site)/book/[bookSlug]/_components/StoreBooksSkeleton.tsx
+++ src/app/(site)/book/[bookSlug]/_components/StoreBooksSkeleton.tsx
@@ -3,18 +3,18 @@ import { Skeleton } from "@nextui-org/react"
 
 import BookWrapper from "@/components/ui/BookWrapper"
 
-interface UserBooksSkeletonProps {}
+interface StoreBooksSkeletonProps {}
 
-const UserBooksSkeleton: FC<UserBooksSkeletonProps> = ({}) => {
+const StoreBooksSkeleton: FC<StoreBooksSkeletonProps> = ({}) => {
     return (
-        <>
+        <div className="grid justify-center">
             <BookWrapper className="h-[165px] w-[115px]">
                 <Skeleton className="h-full w-full" />
             </BookWrapper>
             <Skeleton className="mt-2 h-3 w-[100px]" />
             <Skeleton className="mt-1 h-2 w-[80px]" />
-        </>
+        </div>
     )
 }
 
-export default UserBooksSkeleton
+export default StoreBooksSkeleton
