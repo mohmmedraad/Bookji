436843ecf73848f3602803349baebf4df8906bd6
unknown <mohammed_raad>
2024-04-03 08:01:35 +0300

    fix: algiment

--- src/app/(site)/book/[bookSlug]/_components/StoreBooksSkeleton.tsx
+++ src/app/(site)/book/[bookSlug]/_components/StoreBooksSkeleton.tsx
@@ -7,13 +7,13 @@ interface StoreBooksSkeletonProps {}
 
 const StoreBooksSkeleton: FC<StoreBooksSkeletonProps> = ({}) => {
     return (
-        <div className="grid justify-center">
+        <>
             <BookWrapper className="h-[165px] w-[115px]">
                 <Skeleton className="h-full w-full" />
             </BookWrapper>
-            <Skeleton className="mt-2 h-3 w-[100px]" />
-            <Skeleton className="mt-1 h-2 w-[80px]" />
-        </div>
+            <Skeleton className="mx-auto mt-2 h-3 w-[100px]" />
+            <Skeleton className="mx-auto mt-1 h-2  w-[80px]" />
+        </>
     )
 }
 
